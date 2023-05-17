bd43b2dd920e971467f7e068a843b3be4ebf0e73
anjupatil <anjupatil42@gmail.com>
2023-05-17 18:35:12 +0530

    submitting code-Wed May 17 2023 18:35:11 GMT+0530 (India Standard Time)

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,13 +20,13 @@ const Calculator = () => {
         setResult('');
       };
   return (
-    <div className='calculator'>
+    <div className='Calculator'>
     <div>Calculator</div>
     <input type="text" name="" id="" value={result} readOnly />
     <div className='btns'>
-        <button onClick={() => handleClick('1')}>1</button>
-        <button onClick={() => handleClick('2')}>2</button>
-        <button onClick={() => handleClick('3')}>3</button>
+        <button  onClick={() => handleClick('1')}>1</button>
+        <button  onClick={() => handleClick('2')}>2</button>
+        <button  onClick={() => handleClick('3')}>3</button>
         <button onClick={() => handleClick('4')}>4</button>
         <button onClick={() => handleClick('5')}>5</button>
         <button onClick={() => handleClick('6')}>6</button>
@@ -34,10 +34,10 @@ const Calculator = () => {
         <button onClick={() => handleClick('8')}>8</button>
         <button onClick={() => handleClick('9')}>9</button>
         <button onClick={() => handleClick('0')}>0</button>
-        <button onClick={() => handleClick('+')}>+</button>
+        <button id='btn-5' onClick={() => handleClick('+')}>+</button>
         <button onClick={() => handleClick('-')}>-</button>
         <button onClick={() => handleClick('*')}>*</button>
-        <button onClick={() => handleClick('/')}>/</button>
+        <button  id='btn-9' onClick={() => handleClick('/')}>/</button>
         <button onClick={calculateResult}>=</button>
         <button onClick={clearResult}>Clear</button>
     

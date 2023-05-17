d5322eac30f77751cb8f3b920782df90daa578f0
anjupatil <anjupatil42@gmail.com>
2023-05-17 18:45:33 +0530

    submitting code-Wed May 17 2023 18:45:32 GMT+0530 (India Standard Time)

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -24,22 +24,22 @@ const Calculator = () => {
     <div>Calculator</div>
     <input type="text" name="" id="" value={result} readOnly />
     <div className='btns'>
-        <button  onClick={() => handleClick('1')}>1</button>
-        <button  onClick={() => handleClick('2')}>2</button>
-        <button  onClick={() => handleClick('3')}>3</button>
-        <button onClick={() => handleClick('4')}>4</button>
-        <button onClick={() => handleClick('5')}>5</button>
-        <button onClick={() => handleClick('6')}>6</button>
-        <button onClick={() => handleClick('7')}>7</button>
-        <button onClick={() => handleClick('8')}>8</button>
-        <button onClick={() => handleClick('9')}>9</button>
-        <button onClick={() => handleClick('0')}>0</button>
+        <button id='btn-1' onClick={() => handleClick('1')}>1</button>
+        <button id='btn-2' onClick={() => handleClick('2')}>2</button>
+        <button id='btn-3' onClick={() => handleClick('3')}>3</button>
+        <button id='btn-4' onClick={() => handleClick('4')}>4</button>
+        <button id='btn-5' onClick={() => handleClick('5')}>5</button>
+        <button id='btn-6' onClick={() => handleClick('6')}>6</button>
+        <button id='btn-7'  onClick={() => handleClick('7')}>7</button>
+        <button id='btn-8' onClick={() => handleClick('8')}>8</button>
+        <button id='btn-9' onClick={() => handleClick('9')}>9</button>
+        <button id='btn-0' onClick={() => handleClick('0')}>0</button>
         <button id='plus' onClick={() => handleClick('+')}>+</button>
-        <button id='' onClick={() => handleClick('-')}>-</button>
-        <button onClick={() => handleClick('*')}>*</button>
-        <button  id='btn-9' onClick={() => handleClick('/')}>/</button>
-        <button onClick={calculateResult}>=</button>
-        <button onClick={clearResult}>Clear</button>
+        <button id='minus' onClick={() => handleClick('-')}>-</button>
+        <button id='multiply' onClick={() => handleClick('*')}>*</button>
+        <button id='division' onClick={() => handleClick('/')}>/</button>
+        <button id='result' onClick={calculateResult}>=</button>
+        <button id='clear' onClick={clearResult}>Clear</button>
     
 
     </div>
